cf7d186a85cd72969af5b6d54de99c6b3a470810
kwakseobang <iii6602@naver.com>
2024-07-22 09:56:32 +0900

    더미데이터 생성

--- LApp/Model/User.swift
+++ LApp/Model/User.swift
@@ -14,3 +14,15 @@ struct User {
     var profileURL: String?
     var description: String?
 }
+
+extension User {
+    static var stub1: User {
+        .init(id: "user1", name: "곽문수")
+    }
+    static var stub2: User {
+        .init(id: "user2", name: "곽태풍")
+    }
+    static var stub3: User {
+        .init(id: "user3", name: "송하영")
+    }
+}
